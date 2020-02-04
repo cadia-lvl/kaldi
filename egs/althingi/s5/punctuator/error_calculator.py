8301eb02b38b8585cb44bb845184ce62b0e307d7
Inga Run <ingarunh@gmail.com>
2020-02-04 08:13:55 +0000

    =fix number expansion

--- egs/althingi/s5/punctuator/error_calculator.py
+++ egs/althingi/s5/punctuator/error_calculator.py
@@ -9,7 +9,8 @@ import data
 import codecs
 import sys
 
-MAPPING = {}#{"!EXCLAMATIONMARK": ".PERIOD", "?QUESTIONMARK": ".PERIOD", ":COLON": ".PERIOD", ";SEMICOLON": ".PERIOD"} # Can be used to estimate 2-class performance for example
+MAPPING = {}  # {"!EXCLAMATIONMARK": ".PERIOD", "?QUESTIONMARK": ".PERIOD", ":COLON": ".PERIOD", ";SEMICOLON": ".PERIOD"} # Can be used to estimate 2-class performance for example
+
 
 def compute_error(target_paths, predicted_paths):
     counter = 0
@@ -36,26 +37,30 @@ def compute_error(target_paths, predicted_paths):
 
             target_stream = target.read().split()
             predicted_stream = predicted.read().split()
-            
+
             while True:
 
                 if data.PUNCTUATION_MAPPING.get(target_stream[t_i], target_stream[t_i]) in data.PUNCTUATION_VOCABULARY:
-                    while data.PUNCTUATION_MAPPING.get(target_stream[t_i], target_stream[t_i]) in data.PUNCTUATION_VOCABULARY: # skip multiple consecutive punctuations
-                        target_punctuation = data.PUNCTUATION_MAPPING.get(target_stream[t_i], target_stream[t_i])
-                        target_punctuation = MAPPING.get(target_punctuation, target_punctuation)
+                    # skip multiple consecutive punctuations
+                    while data.PUNCTUATION_MAPPING.get(target_stream[t_i], target_stream[t_i]) in data.PUNCTUATION_VOCABULARY:
+                        target_punctuation = data.PUNCTUATION_MAPPING.get(
+                            target_stream[t_i], target_stream[t_i])
+                        target_punctuation = MAPPING.get(
+                            target_punctuation, target_punctuation)
                         t_i += 1
                 else:
                     target_punctuation = " "
 
                 if predicted_stream[p_i] in data.PUNCTUATION_VOCABULARY:
-                    predicted_punctuation = MAPPING.get(predicted_stream[p_i], predicted_stream[p_i])
+                    predicted_punctuation = MAPPING.get(
+                        predicted_stream[p_i], predicted_stream[p_i])
                     p_i += 1
                 else:
                     predicted_punctuation = " "
 
                 is_correct = target_punctuation == predicted_punctuation
 
-                counter += 1 
+                counter += 1
                 total_correct += is_correct
 
                 if predicted_punctuation == " " and target_punctuation != " ":
@@ -67,16 +72,19 @@ def compute_error(target_paths, predicted_paths):
                 elif predicted_punctuation != " " and target_punctuation != " " and predicted_punctuation != target_punctuation:
                     substitutions += 1
 
-                true_positives[target_punctuation] = true_positives.get(target_punctuation, 0.) + float(is_correct)
-                false_positives[predicted_punctuation] = false_positives.get(predicted_punctuation, 0.) + float(not is_correct)
-                false_negatives[target_punctuation] = false_negatives.get(target_punctuation, 0.) + float(not is_correct)
+                true_positives[target_punctuation] = true_positives.get(
+                    target_punctuation, 0.) + float(is_correct)
+                false_positives[predicted_punctuation] = false_positives.get(
+                    predicted_punctuation, 0.) + float(not is_correct)
+                false_negatives[target_punctuation] = false_negatives.get(
+                    target_punctuation, 0.) + float(not is_correct)
 
                 assert target_stream[t_i] == predicted_stream[p_i] or predicted_stream[p_i] == "<unk>", \
-                       ("File: %s \n" + \
-                       "Error: %s (%s) != %s (%s) \n" + \
-                       "Target context: %s \n" + \
-                       "Predicted context: %s") % \
-                       (target_path,
+                    ("File: %s \n" +
+                     "Error: %s (%s) != %s (%s) \n" +
+                        "Target context: %s \n" +
+                        "Predicted context: %s") % \
+                    (target_path,
                         target_stream[t_i], t_i, predicted_stream[p_i], p_i,
                         " ".join(target_stream[t_i-2:t_i+2]),
                         " ".join(predicted_stream[p_i-2:p_i+2]))
@@ -92,26 +100,29 @@ def compute_error(target_paths, predicted_paths):
     overall_fn = 0.0
 
     print "-"*46
-    print "{:<16} {:<9} {:<9} {:<9}".format('PUNCTUATION','PRECISION','RECALL','F-SCORE')
+    print "{:<16} {:<9} {:<9} {:<9}".format('PUNCTUATION', 'PRECISION', 'RECALL', 'F-SCORE')
     for p in data.PUNCTUATION_VOCABULARY:
 
         if p == data.SPACE:
             continue
 
-        overall_tp += true_positives.get(p,0.)
-        overall_fp += false_positives.get(p,0.)
-        overall_fn += false_negatives.get(p,0.)
+        overall_tp += true_positives.get(p, 0.)
+        overall_fp += false_positives.get(p, 0.)
+        overall_fn += false_negatives.get(p, 0.)
 
         punctuation = p
-        precision = (true_positives.get(p,0.) / (true_positives.get(p,0.) + false_positives[p])) if p in false_positives else nan
-        recall = (true_positives.get(p,0.) / (true_positives.get(p,0.) + false_negatives[p])) if p in false_negatives else nan
-        f_score = (2. * precision * recall / (precision + recall)) if (precision + recall) > 0 else nan        
-        print u"{:<16} {:<9} {:<9} {:<9}".format(punctuation, round(precision,3)*100, round(recall,3)*100, round(f_score,3)*100).encode('utf-8')
+        precision = (true_positives.get(p, 0.) / (true_positives.get(p,
+                                                                     0.) + false_positives[p])) if p in false_positives else nan
+        recall = (true_positives.get(p, 0.) / (true_positives.get(p,
+                                                                  0.) + false_negatives[p])) if p in false_negatives else nan
+        f_score = (2. * precision * recall / (precision + recall)
+                   ) if (precision + recall) > 0 else nan
+        print u"{:<16} {:<9} {:<9} {:<9}".format(punctuation, round(precision, 3)*100, round(recall, 3)*100, round(f_score, 3)*100).encode('utf-8')
     print "-"*46
     pre = overall_tp/(overall_tp+overall_fp) if overall_fp else nan
     rec = overall_tp/(overall_tp+overall_fn) if overall_fn else nan
     f1 = (2.*pre*rec)/(pre+rec) if (pre + rec) else nan
-    print "{:<16} {:<9} {:<9} {:<9}".format("Overall", round(pre,3)*100, round(rec,3)*100, round(f1,3)*100)
+    print "{:<16} {:<9} {:<9} {:<9}".format("Overall", round(pre, 3)*100, round(rec, 3)*100, round(f1, 3)*100)
     print "Err: %s%%" % round((100.0 - float(total_correct) / float(counter-1) * 100.0), 2)
     print "SER: %s%%" % round((substitutions + deletions + insertions) / (correct + substitutions + deletions) * 100, 1)
 
@@ -128,5 +139,4 @@ if __name__ == "__main__":
     else:
         sys.exit("Model predictions file path argument missing")
 
-    compute_error([target_path], [predicted_path])    
-        
+    compute_error([target_path], [predicted_path])
